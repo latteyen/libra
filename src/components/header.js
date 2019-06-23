b98cbcb6114cf10245712f2e5fce2758c6bc4caa
latteyen <38789408+latteyen@users.noreply.github.com>
2019-06-23 18:08:04 +0700

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -77,7 +77,7 @@ const Header = ({ siteTitle }) => (
     <Content>
       <p>
         <HomeLink to="/">{siteTitle}</HomeLink>
-       {/* A comment that won't be rendered  <NavLink to="/blog">Blog</NavLink>
+       {/* A comment that won't be rendered  <NavLink to="http://freelibra.xyz">Freelibra.xyz</NavLink>
         <GitHubLink href="https://github.com/niklasmtj/gatsby-starter-julia">GitHub</GitHubLink> */}
       </p>
     </Content>
