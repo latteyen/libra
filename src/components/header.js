10243ead3921f06f08fc4b1ac02dcc70bbc00886
latteyen <38789408+latteyen@users.noreply.github.com>
2019-06-23 15:23:09 +0700

    hide links

--- src/components/header.js
+++ src/components/header.js
@@ -77,8 +77,8 @@ const Header = ({ siteTitle }) => (
     <Content>
       <p>
         <HomeLink to="/">{siteTitle}</HomeLink>
-        <NavLink to="/blog">Blog</NavLink>
-        <GitHubLink href="https://github.com/niklasmtj/gatsby-starter-julia">GitHub</GitHubLink>
+       {/* A comment that won't be rendered  <NavLink to="/blog">Blog</NavLink>
+        <GitHubLink href="https://github.com/niklasmtj/gatsby-starter-julia">GitHub</GitHubLink> */}
       </p>
     </Content>
   </SiteHeader>
