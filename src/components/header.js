9c75950fe2e55b51f35edca41168347f6dfe9d2a
latteyen <38789408+latteyen@users.noreply.github.com>
2019-06-23 16:02:00 +0700

    Site Title

--- src/components/header.js
+++ src/components/header.js
@@ -76,7 +76,7 @@ const Header = ({ siteTitle }) => (
   <SiteHeader>
     <Content>
       <p>
-        <HomeLink to="/">{siteTitle}</HomeLink>
+        <HomeLink to="/">Libraprice.co</HomeLink>
        {/* A comment that won't be rendered  <NavLink to="/blog">Blog</NavLink>
         <GitHubLink href="https://github.com/niklasmtj/gatsby-starter-julia">GitHub</GitHubLink> */}
       </p>
