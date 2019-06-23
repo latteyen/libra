5760b4a1a594db2f34509765c4835607f9c99a26
latteyen <38789408+latteyen@users.noreply.github.com>
2019-06-23 15:47:29 +0700

    update SEO

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <SEO title="Libra Price" keywords={[`libra`, `libra price`, `libra coin`]} />
     <LandingBio />
   </Layout>
 )
